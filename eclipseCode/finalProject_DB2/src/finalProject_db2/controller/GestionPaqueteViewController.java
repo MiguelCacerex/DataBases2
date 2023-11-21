5e5475727ea3bc87db2a2715d07887875747cb1e
Xime028 <ximemontoya28@gmail.com>
2023-11-21 16:44:19 -0500

    creación de controladores gestion de administrador

---
+++ eclipseCode/finalProject_DB2/src/finalProject_db2/controller/GestionPaqueteViewController.java
@@ -0,0 +1,158 @@
+package finalProject_db2.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+
+public class GestionPaqueteViewController {
+
+    @FXML
+    private Button btnActualizarPaquete;
+
+    @FXML
+    private Button btnActualizarProveedor;
+
+    @FXML
+    private Button btnAgregarPaquete;
+
+    @FXML
+    private Button btnAgregarProveedor;
+
+    @FXML
+    private Button btnEliminarPaquete;
+
+    @FXML
+    private Button btnEliminarProveedor;
+
+    @FXML
+    private Button btnNuevoPaquete;
+
+    @FXML
+    private Button btnNuevoProveedor;
+
+    @FXML
+    private TableColumn<?, ?> columCiudadPaquete;
+
+    @FXML
+    private TableColumn<?, ?> columCodigoPaquete;
+
+    @FXML
+    private TableColumn<?, ?> columCodigoProveedor;
+
+    @FXML
+    private TableColumn<?, ?> columDescripcionPaquete;
+
+    @FXML
+    private TableColumn<?, ?> columDescripcionProveedor;
+
+    @FXML
+    private TableColumn<?, ?> columNombreProveedor;
+
+    @FXML
+    private TableColumn<?, ?> columPrecioPersonaPaquete;
+
+    @FXML
+    private TableColumn<?, ?> columProveedorPaquete;
+
+    @FXML
+    private TableColumn<?, ?> columTipoPaquete;
+
+    @FXML
+    private ComboBox<?> comboCiudadPaquete;
+
+    @FXML
+    private ComboBox<?> comboProveedorPaquete;
+
+    @FXML
+    private ComboBox<?> comboTipoPaquete;
+
+    @FXML
+    private ImageView imagenPaquete;
+
+    @FXML
+    private Tab tabPaquetes;
+
+    @FXML
+    private Tab tabProveedores;
+
+    @FXML
+    private TableView<?> tablePaquetes;
+
+    @FXML
+    private TableView<?> tableProveedores;
+
+    @FXML
+    private TextArea txtDescripcionPaquete;
+
+    @FXML
+    private TextArea txtDescripcionProveedor;
+
+    @FXML
+    private TextField txtNombreProveedor;
+
+    @FXML
+    private TextField txtPrecioPersonaPaquete;
+
+    @FXML
+    void actualizarPaqueteAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void actualizarProveedorAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void agregarPaqueteAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void agregarProveedorAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void ciudadPaqueteAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void eliminarPaqueteAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void eliminarProveedorAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void nuevoPaqueteAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void nuevoProveedorAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void proveedorPaqueteAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void tipoPaqueteAction(ActionEvent event) {
+
+    }
+
+}
---
+++ eclipseCode/finalProject_DB2/src/finalProject_db2/controller/GestionServicioAdicionalViewController.java
@@ -0,0 +1,193 @@
+package finalProject_db2.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+
+public class GestionServicioAdicionalViewController {
+
+    @FXML
+    private Button btnActualizarAutomovil;
+
+    @FXML
+    private Button btnActualizarServicioAdicional;
+
+    @FXML
+    private Button btnAgregarAutomovil;
+
+    @FXML
+    private Button btnAgregarServicioAdicional;
+
+    @FXML
+    private Button btnEliminarAutomovil;
+
+    @FXML
+    private Button btnEliminarServicioAdicional;
+
+    @FXML
+    private Button btnNuevoAutomovil;
+
+    @FXML
+    private Button btnNuevoServicioAdicional;
+
+    @FXML
+    private TableColumn<?, ?> columCapacidadAutomovil;
+
+    @FXML
+    private TableColumn<?, ?> columCodigoAutomovil;
+
+    @FXML
+    private TableColumn<?, ?> columCodigoServicioAdicional;
+
+    @FXML
+    private TableColumn<?, ?> columColorAutomovil;
+
+    @FXML
+    private TableColumn<?, ?> columDescripcionServicioAdicional;
+
+    @FXML
+    private TableColumn<?, ?> columEstadoAutomovil;
+
+    @FXML
+    private TableColumn<?, ?> columGamaAutomovil;
+
+    @FXML
+    private TableColumn<?, ?> columMarcaAutomovil;
+
+    @FXML
+    private TableColumn<?, ?> columNombreServicioAdicional;
+
+    @FXML
+    private TableColumn<?, ?> columNumPuestosAutomoviles;
+
+    @FXML
+    private TableColumn<?, ?> columPlacaAutomovil;
+
+    @FXML
+    private TableColumn<?, ?> columPrecioRentaDiaAutomoviles;
+
+    @FXML
+    private TableColumn<?, ?> columServicioAdicional;
+
+    @FXML
+    private TableColumn<?, ?> columTipoAutomovil;
+
+    @FXML
+    private ComboBox<?> comboEstadoAutomovil;
+
+    @FXML
+    private ComboBox<?> comboGamaAutomovil;
+
+    @FXML
+    private ComboBox<?> comboMarcaAutomovil;
+
+    @FXML
+    private ComboBox<?> comboTipoAutomovil;
+
+    @FXML
+    private ImageView imagenAutomovil;
+
+    @FXML
+    private Tab tabAutomoviles;
+
+    @FXML
+    private Tab tabServiciosAdicionales;
+
+    @FXML
+    private TableView<?> tableAutomoviles;
+
+    @FXML
+    private TableView<?> tableServiciosAdicionales;
+
+    @FXML
+    private TextField txtColorAutomovil;
+
+    @FXML
+    private TextArea txtDescripcionServicioAdicional;
+
+    @FXML
+    private TextField txtNombreServicioAdicional;
+
+    @FXML
+    private TextField txtNumPuestosAutomovil;
+
+    @FXML
+    private TextField txtPlacaAutomovil;
+
+    @FXML
+    private TextField txtPrecioRentaDiaAutomovil;
+
+    @FXML
+    private TextField txtPrecioServicioAdicional;
+
+    @FXML
+    private TextField txtTipoCombustibleAutomovil;
+
+    @FXML
+    void actualizarAutomovilAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void actualizarServicioAdicionalAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void agregarAutomovilAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void agregarServicioAdicionalAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void eliminarAutomovilAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void eliminarServicioAdicionalAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void estadoAutomovilAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void gamaAutomovilAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void marcaAutomovilAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void nuevoAutomovilAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void nuevoServicioAdicionalAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void tipoAutomovilAction(ActionEvent event) {
+
+    }
+
+}
